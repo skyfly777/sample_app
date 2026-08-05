3f5f830a7401c9411bcdb868b0a9e4faef497df1
Jelena <jecazlatanovic99@gmail.com>
2026-08-06 00:38:14 +0200

    Temporarily remove system test job and fix CI lint

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "ExampleUser", email: "user@example.com", password: "foobar", password_confirmation: "foobar" )
+    @user = User.new(name: "ExampleUser", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
