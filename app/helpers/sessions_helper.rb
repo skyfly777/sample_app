7d676e1f799b20ddc5d07aae887b2c1acd70e74b
Jelena <jecazlatanovic99@gmail.com>
2026-08-12 02:06:45 +0200

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,11 +2,26 @@ module SessionsHelper
     # Logs in the given user.
     def log_in(user)
         session[:user_id] = user.id
+        # Guard against session replay attacks.
+        # See https://bit.ly/33UvK0w for more.
+        session[:session_token] = user.session_token
     end
-    # Returns the current logged-in user (if any)
+    # Returns the user corresponding to the remember token cookie.
     def current_user
-        if session[:user_id]
-            @current_user ||= User.find_by(id: session[:user_id])
+        if (user_id = session[:user_id])
+            user = User.find_by(id: user_id)
+
+            if user && session[:session_token] == user.remember_digest
+            @current_user = user
+            end
+
+        elsif (user_id = cookies.encrypted[:user_id])
+            user = User.find_by(id: user_id)
+
+            if user && user.authenticated?(cookies[:remember_token])
+            log_in user
+            @current_user = user
+            end
         end
     end
 
@@ -16,7 +31,21 @@ module SessionsHelper
     end
     # Logs out the current user.
     def log_out
+        forget(current_user)
         reset_session
         @current_user = nil
+    end
+    # Remembers a user in a persistent session.
+    def remember(user)
+        user.remember
+        cookies.permanent.encrypted[:user_id] = user.id
+        cookies.permanent[:remember_token] = user.remember_token
+    end
+
+    # Forgets a persistent session.
+    def forget(user)
+        user.forget
+        cookies.delete(:user_id)
+        cookies.delete(:remember_token)
     end
 end
